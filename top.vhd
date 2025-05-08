b679fee98db60ef2b10db18cc6d79c8324d2317a
UkaszC <Lukasz.cie.2003@gmail.com>
2025-05-09 00:34:20 +0200

    Update top.vhd

--- top.vhd
+++ top.vhd
@@ -11,23 +11,23 @@ architecture Behavioral of top is
     constant N : natural := 100000000; 
 
     signal counter : natural := 0;
-    signal cos : std_logic := '0';
+    signal a : std_logic := '0';
 begin
     multi : process(clk_i, rst_i)
         begin
             if rst_i = '1' then     
-                cos <= '0';
+                a <= '0';
                 counter <= 0;
             elsif counter = N then
                 counter <= 0;
             elsif falling_edge(clk_i) then
                 counter <= counter + 1;     
                 if counter < N/2 then   
-                    cos <= '1';
+                    a <= '1';
                 else
-                    cos <= '0';
+                    a <= '0';
                 end if;
             end if;
     end process multi;
-    led_o <= cos;
+    led_o <= a;
 end Behavioral; 
